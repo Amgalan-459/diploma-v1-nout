a26245948ffcf322c66fd52e8e720a7394f75658
Amgalan <amborgolov@mail.ru>
2025-09-13 01:47:20 +0300

    fixed

--- src/app/core/interfaces/course.ts
+++ src/app/core/interfaces/course.ts
@@ -1,10 +1,11 @@
 export interface Course {
-    id: number;
-    title: string;
-    author: string;
-    rating: number;
-    progressText: string;
-    type: string;
-    image: string;
-    isBuyed: boolean;
+    id: number,
+    title: string,
+    author: string,
+    rating: number,
+    progressText: string,
+    type: string,
+    image: string,
+    isBuyed: boolean,
+    instructions: string
 }
